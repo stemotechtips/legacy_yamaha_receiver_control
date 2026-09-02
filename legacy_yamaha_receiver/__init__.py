aa790657012ef65e57e5d6f2813279a5d26ad0cd
Stephen Moore <tastysmores@gmail.com>
2026-09-02 22:50:58 +1000

    Included 'Zone' as a publicly available class in the package's __init__.py file.

--- legacy_yamaha_receiver/__init__.py
+++ legacy_yamaha_receiver/__init__.py
@@ -1,11 +1,12 @@
 """Python interface for compatible Yamaha receivers."""
 
 from .enums import Audio_Setting_Type, Device_Type, Input_Type
-from .receiver_system import Receiver
+from .receiver_system import Receiver, Zone
 
 __all__ = [
     "Audio_Setting_Type",
     "Device_Type",
     "Input_Type",
     "Receiver",
+    "Zone",
 ]
